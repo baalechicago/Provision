64ac99d74ee5bc3e9330af4771f7082e8cb4fe6e
Noble <ademoyeidowu@gmail.com>
2023-02-16 06:43:28 -0600

    Cloud-init Provision via Terraform CLI

---
+++ .terraform.lock.hcl
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:aa70de7eea84c32a3bc78427d40390bbd4bcaba01a5141f6ddf977db1b6ca446
+size 2305
---
+++ main.tf
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:9c4dc3da99ae65baea2d7c6ab383d4488280889a6b5a3d301b98b3865ed03fc8
+size 2459
